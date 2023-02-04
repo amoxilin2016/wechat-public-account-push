02b49373d2f3892aa2646f0c4e4772a464df8ce1
amoxilin2016 <107381394+amoxilin2016@users.noreply.github.com>
2023-02-04 17:37:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,7 +17,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oUg985itrOTozxcg6-3-TQ69ZAsU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'aU0aiLqb1VxeCduHU4u5IrTDCid5nlqKXyeVwNTVgxM',
+      useTemplateId: '6p9rAzHTaKj9s-Dwn1EnNH9GgmWUPpWeCtIR38TOH8o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-28',
       festivals: [
